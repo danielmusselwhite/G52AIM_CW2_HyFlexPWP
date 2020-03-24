566dfa9288becd02d39e93ff3878ee83f8acd890
Daniel Musselwhite <daniel.l.musselwhite@gmail.com>
2020-03-24 13:15:56 +0000

    Added intensity of mutation to the crossover operators

--- src/com/aim/project/pwp/heuristics/CX.java
+++ src/com/aim/project/pwp/heuristics/CX.java
@@ -29,35 +29,94 @@ public class CX implements XOHeuristicInterface {
 	public double apply(PWPSolutionInterface p1, PWPSolutionInterface p2,
 			PWPSolutionInterface c, double depthOfSearch, double intensityOfMutation) {
 		
+		// rule is iterations = 2(n+1) where n = number of 0.2's there are in IOM
+		//by dividing IOM by 0.2 you have n
+		//int iterations = (int) (2*((intensityOfMutation)/0.2d)+1);
+		int iterations=0;
+		
+		if(intensityOfMutation >= 0 && intensityOfMutation<0.2)
+			iterations = 1;
+		else if(intensityOfMutation >= 0.2 && intensityOfMutation <0.4)
+			iterations = 2;
+		else if(intensityOfMutation >= 0.4 && intensityOfMutation<0.6)
+			iterations = 3;
+		else if(intensityOfMutation >= 0.6 && intensityOfMutation<0.8)
+			iterations = 4;
+		else if(intensityOfMutation >=0.8 && intensityOfMutation<1)
+			iterations = 5;
+		else if(intensityOfMutation == 1)
+			iterations = 6;
+		
+		
+		
 		int[] parent1Solution = p1.getSolutionRepresentation().getSolutionRepresentation();
 		int[] parent2Solution = p2.getSolutionRepresentation().getSolutionRepresentation();
-		
+		int[] childSolution1 = parent1Solution;
+		int[] childSolution2 = parent2Solution;
 
-		
-		// defaulting the child solution to have all the values from parent 2
-		int[] childSolution = parent2Solution;
-		
-		//picking a random index to be the first one for us to explore
-		int nextIndex = oRandom.nextInt(p1.getSolutionRepresentation().getSolutionRepresentation().length);
-		//setting up the cycle the start point for the cycle
-		ArrayList<Integer> cycle = new ArrayList<Integer>();
-		
-		// Finding the cycle and adding it to the child
-		//whilst the cycle doesn't contain the next element..
-		while(!cycle.contains(parent1Solution[nextIndex])) { 
-			// add this element to the child at its index
-			childSolution[nextIndex] = parent1Solution[nextIndex];
-			// add this element to the cycle
-			cycle.add(parent1Solution[nextIndex]);
+		for(int i=0; i<iterations; i++){
+			//GENERATING CHILD 1
 			
-			//getting where the element at this index is in parent two, in parent one.
-			nextIndex = Utilities.arrayGetIndexOf(parent1Solution, parent2Solution[nextIndex]);
+			// defaulting the child1 solution to have all the values from parent 2
+			childSolution1 = parent2Solution;
+			
+			
+			//picking a random index to be the first one for us to explore
+			int nextIndex = oRandom.nextInt(p1.getSolutionRepresentation().getSolutionRepresentation().length);
+			//setting up the cycle the start point for the cycle
+			ArrayList<Integer> cycle = new ArrayList<Integer>();
+			
+			// Finding the cycle and adding it to the child
+			//whilst the cycle doesn't contain the next element..
+			while(!cycle.contains(parent1Solution[nextIndex])) { 
+				// add this element to the child at its index
+				childSolution1[nextIndex] = parent1Solution[nextIndex];
+				// add this element to the cycle
+				cycle.add(parent1Solution[nextIndex]);
+				
+				//getting where the element at this index is in parent two, in parent one.
+				nextIndex = Utilities.arrayGetIndexOf(parent1Solution, parent2Solution[nextIndex]);
 
+			}
+			
+			
+			//GENERATING CHILD 2
+			
+			// defaulting the child1 solution to have all the values from parent 2
+			childSolution2 = parent1Solution;
+			
+			
+			//picking a random index to be the first one for us to explore
+			nextIndex = oRandom.nextInt(p1.getSolutionRepresentation().getSolutionRepresentation().length);
+			//setting up the cycle the start point for the cycle
+			cycle = new ArrayList<Integer>();
+			
+			// Finding the cycle and adding it to the child
+			//whilst the cycle doesn't contain the next element..
+			while(!cycle.contains(parent2Solution[nextIndex])) { 
+				// add this element to the child at its index
+				childSolution2[nextIndex] = parent2Solution[nextIndex];
+				// add this element to the cycle
+				cycle.add(parent2Solution[nextIndex]);
+				
+				//getting where the element at this index is in parent 1, in parent 2.
+				nextIndex = Utilities.arrayGetIndexOf(parent2Solution, parent1Solution[nextIndex]);
+
+			}
+			
+			//setting the parents to be the children
+			parent1Solution = childSolution1;
+			parent2Solution = childSolution2;
 		}
 		
-		//setting the child to the new solution representation
-		c.getSolutionRepresentation().setSolutionRepresentation(childSolution);  // move to the new solution
 		
+		
+		//setting the child to be randomly 1 of the 2 generated children (50/50 chance of being either)
+		if(oRandom.nextDouble()>0.5)
+			c.getSolutionRepresentation().setSolutionRepresentation(childSolution1);  // move to the new solution
+		else
+			c.getSolutionRepresentation().setSolutionRepresentation(childSolution2);  // move to the new solution	
+	
 		return c.getObjectiveFunctionValue();
 	}
 
--- src/com/aim/project/pwp/heuristics/OX.java
+++ src/com/aim/project/pwp/heuristics/OX.java
@@ -34,53 +34,114 @@ public class OX implements XOHeuristicInterface {
 	public double apply(PWPSolutionInterface p1, PWPSolutionInterface p2,
 			PWPSolutionInterface c, double depthOfSearch, double intensityOfMutation) {
 	
+		// rule is iterations = 2(n+1) where n = number of 0.2's there are in IOM
+		//by dividing IOM by 0.2 you have n
+		//int iterations = (int) (2*((intensityOfMutation)/0.2d)+1);
+		int iterations=0;
+		
+		if(intensityOfMutation >= 0 && intensityOfMutation<0.2)
+			iterations = 1;
+		else if(intensityOfMutation >= 0.2 && intensityOfMutation <0.4)
+			iterations = 2;
+		else if(intensityOfMutation >= 0.4 && intensityOfMutation<0.6)
+			iterations = 3;
+		else if(intensityOfMutation >= 0.6 && intensityOfMutation<0.8)
+			iterations = 4;
+		else if(intensityOfMutation >=0.8 && intensityOfMutation<1)
+			iterations = 5;
+		else if(intensityOfMutation == 1)
+			iterations = 6;
+		
 		int size = p1.getNumberOfLocations()-2; //we can't modify the home nor the depot so minus 2 from total
-
 		int cutPoint1;
 		int cutPoint2;
 		
-		// pick two random cut points while both the points are the first and the last element as nothing will change or whilst they are the same element
-		do {
-			int point1 = oRandom.nextInt(size);
-			int point2 = oRandom.nextInt();
-			
-			// setting it so cutPoint1 is the smaller and cutPoint2 is the bigger
-			cutPoint1 = point1<point2 ? point1 : point2;
-			cutPoint2 = point2>point1 ? point2 : point1;
-		} while((cutPoint1 == 0 && cutPoint2 == size-1) || cutPoint1 == cutPoint2);
-		
-		
-		
-		// perform OX maintaining the order
-		
-		int[] childSolutionRepresentation = new int[c.getSolutionRepresentation().getSolutionRepresentation().length];
+		int[] child1SolutionRepresentation = new int[size];
+		int[] child2SolutionRepresentation = new int[size];
 		int[] parent1SolutionRepresentation = p1.getSolutionRepresentation().getSolutionRepresentation();
-		int[] parent2SolutionRepresentation = Utilities.shiftArray(p2.getSolutionRepresentation().getSolutionRepresentation(), cutPoint2); //shifting it by cutPoint2 so it is read in that order
+		int[] parent2SolutionRepresentation = p2.getSolutionRepresentation().getSolutionRepresentation();
 		
-		// Copy parent 1 middle into child
-		for(int i=cutPoint1; i<cutPoint2; i++) {
-			childSolutionRepresentation[i] = parent1SolutionRepresentation[i];
-		}
-		
-		//get symbols from parent 2 starting from second cut point
-		int[] parent2Symbols = new int[cutPoint2 - cutPoint1];
-		
-		// for each symbol from parent 2
-		for(int i=0; i<parent2Symbols.length ;i++) {
-			//if the child doesn't contain this value already (its not from parent 1 middle)
-			if(!Utilities.arrayContainsValue(childSolutionRepresentation, parent2Symbols[i]))
-				parent2Symbols[i] = parent2SolutionRepresentation[i]; //adding this symbol to the parent2Symbols
+		for(int j=0; j<iterations; j++) {
+			
+			
+			// pick two random cut points while both the points are the first and the last element as nothing will change or whilst they are the same element
+			do {
+				int point1 = oRandom.nextInt(size);
+				int point2 = oRandom.nextInt();
 				
+				// setting it so cutPoint1 is the smaller and cutPoint2 is the bigger
+				cutPoint1 = point1<point2 ? point1 : point2;
+				cutPoint2 = point2>point1 ? point2 : point1;
+			} while((cutPoint1 == 0 && cutPoint2 == size-1) || cutPoint1 == cutPoint2);
+			
+			
+			
+			// perform OX maintaining the order
+			
+			child1SolutionRepresentation = new int[size];
+			child2SolutionRepresentation = new int[size];
+			parent2SolutionRepresentation = Utilities.shiftArray(parent2SolutionRepresentation, cutPoint2); //shifting it by cutPoint2 so it is read in that order
+			
+			
+			//GENERATING CHILD 1
+			
+			// Copy parent 1 middle into child
+			for(int i=cutPoint1; i<cutPoint2; i++) {
+				child1SolutionRepresentation[i] = parent1SolutionRepresentation[i];
+			}
+			
+			//get symbols from parent 2 starting from second cut point
+			int[] parent2Symbols = new int[cutPoint2 - cutPoint1];
+			
+			// for each symbol from parent 2
+			for(int i=0; i<parent2Symbols.length ;i++) {
+				//if the child doesn't contain this value already (its not from parent 1 middle)
+				if(!Utilities.arrayContainsValue(child1SolutionRepresentation, parent2Symbols[i]))
+					parent2Symbols[i] = parent2SolutionRepresentation[i]; //adding this symbol to the parent2Symbols
+					
+			}
+			
+			// for each in the parent 2 symbols, wrap them around the child in order from the second cutpoint
+			for(int i=0; i<parent2Symbols.length; i++) {
+				child1SolutionRepresentation[(i+cutPoint2)%size] = parent2Symbols[i];
+			}
+			
+			
+			
+			
+			//GERNEATING CHILD 2
+			// Copy parent 2 middle into child
+			for(int i=cutPoint1; i<cutPoint2; i++) {
+				child2SolutionRepresentation[i] = parent2SolutionRepresentation[i];
+			}
+			
+			//get symbols from parent 1 starting from second cut point
+			int[] parent1Symbols = new int[cutPoint2 - cutPoint1];
+			
+			// for each symbol from parent 1
+			for(int i=0; i<parent1Symbols.length ;i++) {
+				//if the child doesn't contain this value already (its not from parent 2 middle)
+				if(!Utilities.arrayContainsValue(child2SolutionRepresentation, parent1Symbols[i]))
+					parent1Symbols[i] = parent1SolutionRepresentation[i]; //adding this symbol to the parent1Symbols
+					
+			}
+			
+			// for each in the parent 2 symbols, wrap them around the child in order from the second cutpoint
+			for(int i=0; i<parent1Symbols.length; i++) {
+				child2SolutionRepresentation[(i+cutPoint2)%size] = parent1Symbols[i];
+			}
+			
+			//setting the parents for the next iteration to be the children
+			parent1SolutionRepresentation = child1SolutionRepresentation;
+			parent2SolutionRepresentation = child2SolutionRepresentation;
 		}
 		
-		// for each in the parent 2 symbols, wrap them around the child in order from the second cutpoint
-		for(int i=0; i<parent2Symbols.length; i++) {
-			childSolutionRepresentation[(i+cutPoint2)%size] = parent2Symbols[i];
-		}
-		
-		//setting the child to the new solution representation
-		c.getSolutionRepresentation().setSolutionRepresentation(childSolutionRepresentation);  // move to the new solution
-		
+		//setting the child to be randomly 1 of the 2 generated children (50/50 chance of being either)
+		if(oRandom.nextDouble()>0.5)
+			c.getSolutionRepresentation().setSolutionRepresentation(child1SolutionRepresentation);  // move to the new solution
+		else
+			c.getSolutionRepresentation().setSolutionRepresentation(child2SolutionRepresentation);  // move to the new solution	
+	
 		return c.getObjectiveFunctionValue();
 	}
 
